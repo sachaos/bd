9fc9b1b1ebf3aee3025cd28035c9ca6aec07ab18
Takumasa Sakao <sakataku7@gmail.com>
2021-02-06 22:21:53 +0900

    Add json output

--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
 )
 
+var output string
+
 func main() {
-	arg := os.Args[1]
+	flag.StringVar(&output, "output", "", "output format")
+	flag.Parse()
+
+	arg := flag.Arg(0)
 	parse, err := Parse(arg)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	Describe(parse)
+	if output == "json" {
+		JsonOut(parse)
+	} else {
+		Describe(parse)
+	}
 }
 
 func Parse(origin string) ([]byte, error) {
@@ -44,4 +55,15 @@ func Describe(b []byte) {
 	fmt.Printf("binary: %08b\n", b)
 	fmt.Println()
 	fmt.Printf("length: %dbit, %dbytes\n", len(b)*8, len(b))
+}
+
+func JsonOut(b []byte) {
+	json.NewEncoder(os.Stdout).Encode(map[string]interface{}{
+		"hex":         hex.EncodeToString(b),
+		"string":      fmt.Sprintf("%s", b),
+		"bytes":       fmt.Sprintf("%v", b),
+		"binary":      fmt.Sprintf("%b", b),
+		"bit_length":  fmt.Sprintf("%d bit", len(b)*8),
+		"byte_length": fmt.Sprintf("%d byte", len(b)),
+	})
 }
